b46d14e42c0af82c87ea7e6e70043190a3aecbb4
f3kilo <f3kilo@yandex.ru>
2020-04-08 08:09:42 +0300

    remove excessed comments in main.rs

--- src/main.rs
+++ src/main.rs
@@ -71,7 +71,7 @@ fn init() -> Result<(Logger, EventLoop<()>, Window, Sender<Input>), InitError> {
     let window = init_window(&event_loop)?;
     trace!(logger, "Window initialized");
 
-    // Init input sender
+    // Init input printer
     let (tx, rx) = channel();
     let input_logger = InputLogger::new(rx, logger.clone());
     std::thread::spawn(|| input_logger.run());
@@ -100,49 +100,3 @@ fn init_logger(save_path: PathBuf) -> Result<Logger, LogInitError> {
         .build()?;
     Ok(logger)
 }
-
-// struct HexFieldPlayground {}
-//
-// impl HexFieldPlayground {
-//     fn new() -> Self {
-//         Self {}
-//     }
-//
-//     fn init() {}
-//
-//     fn run() {}
-// }
-
-// fn run() {
-//     let logger = init_logger();
-//
-//     let event_loop = EventLoop::new();
-//     let window_builder = WindowBuilder::default()
-//         .with_title("Test window")
-//         .with_inner_size(PhysicalSize::new(1920, 1080));
-//     let window = window_builder
-//         .build(&event_loop)
-//         .expect("Can't create window");
-//     println!("Run");
-//     event_loop.run(move |event, _, control_flow| {
-//         *control_flow = ControlFlow::Poll;
-//         match event {
-//             Event::WindowEvent {
-//                 event: WindowEvent::CloseRequested,
-//                 ..
-//             } => {
-//                 println!("Close event");
-//                 *control_flow = ControlFlow::Exit;
-//             }
-//             Event::DeviceEvent(_, event) => {}
-//             Event::MainEventsCleared => {
-//                 window.request_redraw();
-//                 println!("request_redraw called");
-//             }
-//             Event::RedrawRequested(_) => {
-//                 println!("RedrawRequested");
-//             }
-//             _ => (),
-//         };
-//     });
-// }
